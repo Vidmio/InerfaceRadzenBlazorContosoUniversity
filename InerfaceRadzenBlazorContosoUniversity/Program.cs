359ce04e9aa6c800660d7262b907a4c3b59c39a8
Vid Mio <vidmio@gmail.com>
2024-01-30 13:32:42 +0100

    setup connection string

--- InerfaceRadzenBlazorContosoUniversity/Program.cs
+++ InerfaceRadzenBlazorContosoUniversity/Program.cs
@@ -1,5 +1,7 @@
 using InerfaceRadzenBlazorContosoUniversity.Components;
+using InerfaceRadzenBlazorContosoUniversity.Model;
 using Radzen;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,11 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddRadzenComponents();
+
+
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DataConnectionString"), sqlServerOptions => sqlServerOptions.CommandTimeout(120)),
+            ServiceLifetime.Transient);
 
 var app = builder.Build();
 
